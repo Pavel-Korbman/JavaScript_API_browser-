741e68a0a1dfb58576598e7124c1bacedd535aa5
Pavel <pavel@b2profit.ru>
2025-03-05 18:38:19 +0300

    1 лекция

---
+++ 1_Lect/navigation_elements.js
@@ -0,0 +1,16 @@
+// Навигация по элементам
+
+// Методы навигации по дочерним узлам: обращаются ко всем узлам - включая перенос текста и комментарии
+// - firstChild 
+// - childNodes
+// - lastChild 
+// - children
+
+// Методы навигации по дочерним элементам: обращаются только к элементам (div span и т.п.)
+// - firstElementChild 
+// - lastElementChild 
+// - previousElementSibling
+// - nextElementSibling 
+// - parentElement
+
+// Свойства узлов
