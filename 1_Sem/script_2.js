d1094aeeb67ee3d84ba5b3848e1b47f011f05ef5
Pavel <pavel@b2profit.ru>
2025-03-11 17:01:11 +0300

    1 семинар

--- 1_Sem/script_2.js
+++ 1_Sem/script_2.js
@@ -6,3 +6,34 @@
 // 4. При нажатии на кнопку "Клонировать элемент" создается копия последнего добавленного элемента и добавляется на страницу. 
 // 5. Все элементы имеют класс .box и стилизованы с помощью CSS (см. пример). 
 // 6. Элементы могут быть добавлены, удалены и клонированы в любом порядке и в любом количестве.
+
+
+const addButton = document.querySelector('.add-button');
+const delButton = document.querySelector('.del-button');
+const cloneButton = document.querySelector('.clone-button');
+const boxes = document.querySelector('.boxes');
+let count = 0;
+
+addButton.addEventListener('click', () => {
+    count++;
+    const box = document.createElement('div');
+    box.classList = 'box';
+    box.textContent = `Элемент № ${count}`;
+    boxes.appendChild(box);
+    // addButton.before(box);
+});
+
+delButton.addEventListener('click', () => {
+    if (count!==0){count--;}
+    if(boxes.querySelector('.box')){
+        boxes.removeChild(boxes.lastElementChild);
+    }    
+});
+
+cloneButton.addEventListener('click', () => {    
+    if(boxes.querySelector('.box')){
+        const clone = boxes.lastElementChild.cloneNode();
+        clone.textContent=boxes.lastElementChild.textContent;
+        boxes.appendChild(clone);
+    }    
+});
