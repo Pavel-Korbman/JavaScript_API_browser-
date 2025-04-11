2accf0b949dc030814fb3107565317ba9cb6c138
Pavel <pavel@b2profit.ru>
2025-04-11 04:03:16 +0300

    2 ДЗ

--- 2_DZ/script.js
+++ 2_DZ/script.js
@@ -1,12 +1,12 @@
-if (!localStorage.getItem('currentPhotoIndex') 
+if (!localStorage.getItem('currentPhotoIndex')
     || localStorage.getItem('currentPhotoIndex') >= photo.length) {
     localStorage.setItem('currentPhotoIndex', 0);
 };
-
+const firstPhotoIndex = localStorage.getItem('currentPhotoIndex');
 const backBtn = document.querySelector('.back');
 const forwardBtn = document.querySelector('.forward');
 const currentPhoto = document.querySelector('.gallery__photo');
-currentPhoto.src=photo[localStorage.getItem('currentPhotoIndex')].url;
+currentPhoto.src = photo[firstPhotoIndex].url;
 const paginationBox = document.querySelector('.pagination__box');
 
 for (let index = 0; index < photo.length; index++) {
@@ -14,28 +14,40 @@ for (let index = 0; index < photo.length; index++) {
     paginationBtn.classList = 'pagination__btn';
     paginationBtn.href = '#';
     paginationBtn.textContent = `${index + 1}`;
-    paginationBox.appendChild(paginationBtn);    
+    paginationBox.appendChild(paginationBtn);
 };
 
-backBtn.addEventListener('click', ()=>{
-    let i = localStorage.getItem('currentPhotoIndex');  
-    (i == 0)? i = photo.length - 1 : i--;    
-    currentPhoto.src = photo[i].url;
-    localStorage.setItem('currentPhotoIndex', i);       
-});
-
-forwardBtn.addEventListener('click', ()=>{
-    let i = localStorage.getItem('currentPhotoIndex');   
-    (i == photo.length - 1)? i = 0 : i++;    
-    currentPhoto.src=photo[i].url;
-    localStorage.setItem('currentPhotoIndex', i);        
-});
-
 const paginationButtons = document.querySelectorAll('.pagination__btn');
+paginationButtons[firstPhotoIndex].classList.add('red');
+
+backBtn.addEventListener('click', () => {
+    let i = localStorage.getItem('currentPhotoIndex');
+    paginationButtons[i].classList.remove('red');
+    (i == 0) ? i = photo.length - 1 : i--;
+    currentPhoto.src = photo[i].url;
+    paginationButtons[i].classList.add('red');
+    localStorage.setItem('currentPhotoIndex', i);
+});
+
+forwardBtn.addEventListener('click', () => {
+    let i = localStorage.getItem('currentPhotoIndex');
+    paginationButtons[i].classList.remove('red');
+    (i == photo.length - 1) ? i = 0 : i++;
+    currentPhoto.src = photo[i].url;
+    paginationButtons[i].classList.add('red');
+    localStorage.setItem('currentPhotoIndex', i);
+});
 
 paginationButtons.forEach(e => {
-    e.addEventListener('click', ()=>{              
-        currentPhoto.src=photo[Number(e.textContent) - 1].url;
-        localStorage.setItem('currentPhotoIndex', Number(e.textContent));
+    e.addEventListener('click', () => {
+        let i = localStorage.getItem('currentPhotoIndex');        
+        let j = Number(e.textContent) - 1;
+        paginationButtons[i].classList.remove('red');          
+        currentPhoto.src = photo[j].url;
+        paginationButtons[j].classList.add('red'); 
+        localStorage.setItem('currentPhotoIndex', Number(e.textContent) - 1); 
     })
 });
+
+
+// localStorage.clear();
