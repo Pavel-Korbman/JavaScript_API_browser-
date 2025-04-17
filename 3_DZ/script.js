f7fe7da2373cbf0fae80a7d21fdec3334188eb22
Pavel <pavel@b2profit.ru>
2025-04-17 09:17:23 +0300

    3 ДЗ

--- 3_DZ/script.js
+++ 3_DZ/script.js
@@ -7,11 +7,14 @@ const likes = document.querySelector('.likes');
 const likedBtn = document.querySelector('.gallery__like-btn');
 let boolean = false;
 
+
 if (!localStorage.getItem('likeCounter')) {
     localStorage.setItem('likeCounter', 0);
 };
 let likeCounter = localStorage.getItem('likeCounter');
 likes.textContent = `Вам понравилось: ${likeCounter} фото`;
+
+let currentPhotoId = ''; //
 
 const currentPhotoLink = '';
 let history = [];
@@ -21,7 +24,7 @@ if (!localStorage.getItem('history')) {
 
 fetch('https://api.unsplash.com/photos/random/?client_id=mMPb5Q6Mo4kh0kOIL1U-CWyD3r0nG09okBkSMzCFTa0')
     .then(response => response.json())
-    .then(obj => {        
+    .then(obj => {
         currentPhoto.src = obj.urls.regular;
         currentDescription.textContent = obj.alt_description
             ;
@@ -29,16 +32,27 @@ fetch('https://api.unsplash.com/photos/random/?client_id=mMPb5Q6Mo4kh0kOIL1U-CWy
         authorBio.textContent = obj.user.bio;
         authorCountry.textContent = obj.user.location
             ;
-            let item = [obj.user.name, obj.links.html];
+
+        currentPhotoId = obj.id; //        
+
+        let item = [obj.user.name, obj.links.html];
         history.splice(0, 0, item);
         localStorage.setItem('history', JSON.stringify(history));
     });
 
 const setLike = (boolean) => {
     if (boolean === true) {
+        // fetch(`https://api.unsplash.com/photos/:${currentPhotoId}/like/?client_id=mMPb5Q6Mo4kh0kOIL1U-CWyD3r0nG09okBkSMzCFTa0`, {
+        //     method: 'POST' 
+        // })
+        //     .then(response => console.log(response.status));
         likedBtn.classList.add('active');
         likeCounter++;
     } else {
+        // fetch(`https://api.unsplash.com/photos/:${currentPhotoId}/like/?client_id=mMPb5Q6Mo4kh0kOIL1U-CWyD3r0nG09okBkSMzCFTa0`, {
+        //     method: 'DELETE'                    
+        // })
+        //     .then(response => console.log(response.status));
         likedBtn.classList.remove('active');
         likeCounter--;
     };
@@ -46,7 +60,7 @@ const setLike = (boolean) => {
     likes.textContent = `Вам понравилось: ${likeCounter} фото`;
 }
 
-likedBtn.addEventListener('click', () => {
+likedBtn.addEventListener('click', () => {    
     boolean = !boolean;
     setLike(boolean);
 });
@@ -54,3 +68,4 @@ likedBtn.addEventListener('click', () => {
 
 
 // localStorage.clear();
+
